4c3626f38b22b49b2f48d2e81198376010f4774e
Kirtan Padh <kirtan.701@gmail.com>
2017-12-12 01:56:11 +0100

    graph update update kind of works

--- clinical_graph/main.js
+++ clinical_graph/main.js
@@ -1,12 +1,92 @@
 "use strict";
 
+
 var graph_data = JSON.parse(data)
 
-document.addEventListener('DOMContentLoaded', function() {
-  var mainUser;
-  var cy = window.cy = cytoscape({
-    container: document.getElementById('cy'),
-    elements: graph_data,
-  });
+
+//document.addEventListener('DOMContentLoaded', function() {
+var cy = cytoscape({
+container: document.getElementById('cy'),
+
+elements: graph_data,
+
+style: [ // the stylesheet for the graph
+	{
+	  selector: 'node',
+	  style: {
+	    'background-color': '#FE642E',
+		'width': '1px',
+        'height': '1px',
+        'border-color': '#fff',
+        'border-width': '0.2px',
+        'border-opacity': 0.7
+	  }
+	},
+
+	{
+	  selector: 'edge',
+	  style: {
+	    'width': 0.1,
+	    'line-color': '#ccc',
+	  }
+	}
+]
 });
+var spring_layout = cy.layout({
+  name: 'cose'
+});
+
+//var nodes = cy.nodes("[gender = 'female']");
+//var eles = nodes.union(cy.edges("[distance < 0.1]"));
+//cy.remove(eles.absoluteComplement());
+
+spring_layout.run()
+
+//});
+
+document.getElementById("dist_thresh").value = '0.1';
+
+
+var removed;
+
+function updateGraph(){
+
+	cy.startBatch();
+	
+//	cy.add(removed);
+	var gender_selection = document.getElementById("input_gender").value;	
+	var race_selection = document.getElementById("input_race").value;
+	var dist_threshold = document.getElementById("dist_thresh").value;
+	
+	if(gender_selection == 'all'){
+		var gender_nodes = cy.nodes();
+	} else {
+		var gender_nodes = cy.nodes('[gender = \'' + gender_selection + '\']');
+	}
+	
+	if(race_selection == 'all'){
+		var race_nodes = cy.nodes();
+	} else {
+		var race_nodes = cy.nodes('[race = \'' + race_selection + '\']');
+	}
+	
+	var keep_nodes = race_nodes.intersection(gender_nodes);
+	var keep_edges = cy.edges('[distance < ' + dist_threshold + ']');
+	
+	var updated_graph = keep_nodes.union(keep_edges);
+	
+	removed = cy.remove(updated_graph.absoluteComplement());
+	
+	cy.endBatch();
+	
+	
+	//var t1 = cy.nodes("[gender = 'female']");
+	//var t2 = t1.union(cy.edges("[distance < 0.05]"));
+	//cy.remove(t2.absoluteComplement());
+	return false;
+}
+
+
+
+
 
