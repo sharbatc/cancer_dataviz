eca0727393a0ca945aad58143d3c6c155e35f6d1
Luis Medina <luis.medinarios@epfl.ch>
2017-12-22 22:33:01 +0100

    .

--- sunburst.js
+++ sunburst.js
@@ -7,10 +7,12 @@ http://tietyk.github.io/D3/
 */
 
 //Defining size of the svg element
-var width = 1500;
-var height = 1300;
+var width = 750;
+var height = 600;
+var offset_x=0;
+var offset_y=0;
 //Defining radius of the circle
-var radius = 280;
+var radius = Math.min(width-150, height) / 2;
 //Defining Scales for x and y axis
 var x = d3.scaleLinear().range([0,2*Math.PI]);
 var y = d3.scaleLinear().range([0,radius]);
@@ -18,7 +20,7 @@ var y = d3.scaleLinear().range([0,radius]);
 //Defining colors for each parent and children used in the sunburst
 var color ={
   "gender":"#ffffff",
-  //Gender:
+  //Gender: 
   "Male":"#fc4e68",
   "Female":"#fdb9c2",
   //Race: Orange
@@ -27,7 +29,7 @@ var color ={
   "Latino":"#fdc24b",
   "Native American":"#fdd481",
   "White":"#feecc9",
-  //Countries:
+  //Countries: 
   "Pakistan":"#254e4e",
   "USA":"#387575",
   "Nigeria":"#4b9c9c",
@@ -44,9 +46,9 @@ var color ={
   "Undefined":"#dcdcdd"
 };
 
-//Defining colors for each parent and children. Note that this variable will be used for the
+//Defining colors for each parent and children. Note that this variable will be used for the 
 var colors_scale = d3.scaleOrdinal()
-    .domain(["Gender", "Male", "Female",
+    .domain(["Gender", "Male", "Female", 
       "Race", "Asian", "Black", "Latino", "Native American", "White",
       "Country","Pakistan", "USA", "Nigeria", "Canada", "Germany", "Brazil", "Poland",
       "Age group", "Under 50", "Between 50 and 60","Between 60 and 70","Above 70",
@@ -59,11 +61,11 @@ var colors_scale = d3.scaleOrdinal()
 
 //Setting up the svg element according to the size
 var svg = d3.select('body').append("svg")
+  .attr("class","sunburst")
   .attr('width', width)
   .attr('height', height)
   .append('g')
-  // .attr("viewBox", "0 0 10 50")
-  .attr('transform', 'translate(' + 650 + ',' + height / 2 + ')');
+  .attr('transform', 'translate(' + (offset_x+width-150) / 2 + ',' + (offset_y+height) / 2 + ')');
 
 //Getting the sizes of the slices of the elements of the sunburst.
 var arc = d3.arc()
@@ -92,15 +94,15 @@ function key_html(d) {
       k.push("<b>"+"Age group: "+"</b>"+d.data.name)
     }
     d = d.parent;
-
+    
   }
   return k.reverse().join(" > ");
 }
 
-//Defining two tooltip variables:
+//Defining two tooltip variables: 
 //One (tooltip_fixed) in case of clicking on an element of the sunburst (will show the percentage (in terms of the size) of the element considering the whole tree)
 //The other (tooltip) used in case of moving through an element (this is moving according the cursor)
-var tooltip_fixed = d3.select("body").append("div").attr("class","tooltip").style("opacity",0);
+var tooltip_fixed = d3.select("body").append("div").attr("class","tooltip_fixed").style("opacity",0);
 var tooltip = d3.select("body").append("div").attr("class","tooltip").style("opacity",0);
 
 //click function that will make a "zoom" over the selected element and will show the tooltip_fixed with the whole selected path and percentage
@@ -116,7 +118,7 @@ function click(d){
   })
     .selectAll("path")
     .attrTween("d",function(d){return function(){ return arc(d);}; });
-  //Showing up the tooltip
+  //Showing up the tooltip 
   //in case of moving through the center, don't show anything, since it doesn't have information (it's only the root "gender")
   if (d.data.name =="gender"){
     d3.select(this).style("cursor","default");
@@ -128,10 +130,6 @@ function click(d){
     var percentage=(100 * d.value / total_size ).toPrecision(3);
     tooltip_fixed.html(key_html(d)+ " > " + "<b>" +percentage + " %</b>")
       .style("opacity",0.8)
-      .style('position','relative')
-      .style('bottom','50%')
-      .style('left','100')
-      .style('text-align','center')
       .style("font-size","15px")
       .style('display', 'block');
   }
@@ -168,7 +166,7 @@ function mouseout(d){
 
 //Adding a static legend with the colors and their meaning
 var legend= d3.select("svg")
-  .attr("class","legend")
+  //.attr("class","legend")
   .append("g")
   .selectAll("g")
   .data(colors_scale.domain())
@@ -176,8 +174,8 @@ var legend= d3.select("svg")
   .append("g")
     .attr("transform", function(d,i) { //Aligning the legend (each of the circles and the text) to the right side of the sunburst
         var legend_height = 20;
-        var x = 1100;
-        var y = i * legend_height+((height-400)/2);
+        var x = (width-125)+offset_x;
+        var y = i * legend_height+((height-400)/2)+offset_y;
         return 'translate(' + x + ',' + y + ')';
   });
 //Appending circles to the legend. These circles will be filled with the color of their respective label
@@ -211,7 +209,7 @@ function render_sunburst(){d3.json("clinical_data_nested_revisited.json",functio
     .style("stroke-opacity",0)
     .style("stroke-width","3px")
     .style("fill", function (d) { return color[d.data.name]; }) //filling the areas of the sunburst with colors according to the color dictionary
-    .style("opacity", 1.0)
+    .style("opacity", 1)
     .on("click",click)
     .on("mouseout",mouseout)
     .on("mousemove",mousemove)
